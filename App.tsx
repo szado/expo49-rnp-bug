7bf7946fbcec113b44f7af791d2fabd0f5a6f518
Jarosław Żak <shado.ackerly@gmail.com>
2023-09-15 16:26:12 +0200

    feat: bug reproduction

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import {PaperProvider, Menu, Button} from 'react-native-paper';
+import * as React from 'react';
 
 export default function App() {
+  const [visible, setVisible] = React.useState(false);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <PaperProvider>
+        <View
+            style={{
+              paddingTop: 50,
+              flexDirection: 'row',
+              justifyContent: 'center',
+            }}>
+          <Menu
+              visible={visible}
+              onDismiss={closeMenu}
+              anchor={<Button onPress={openMenu}>Show menu</Button>}>
+            <Menu.Item onPress={() => {}} title="Item 1" />
+            <Menu.Item onPress={() => {}} title="Item 2" />
+            <Menu.Item onPress={() => {}} title="Item 3" />
+          </Menu>
+        </View>
+      </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
